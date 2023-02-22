fd4366df48ed0ca493d755c27278e4092db914a6
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-02-22 21:14:10 +0100

    IMPR: More efficient caching

--- src/init.sh
+++ src/init.sh
@@ -2,16 +2,20 @@
 
 function init()
 {
-    mkdir -p .judo/plugins_dir
-    source $DATA_ROOT/src/plugins.sh; echo $(_active_plugins) | xargs -I{} cp -rp $DATA_ROOT/plugins/{} .judo/plugins_dir
-    mkdir -p .judo
+    mkdir -p .judo/.cache .judo/.cache/plugins_dir
+    # Get active plugins and copy to image cache
+    source $DATA_ROOT/src/plugins.sh; echo $(_active_plugins) | xargs -I{} cp -rpu $DATA_ROOT/plugins/{} .judo/.cache/plugins_dir
     touch .judo/config.sh .judo/plugins
-    cp $JUDO_CONFIG_HOME/config.sh .judo/combined_config.sh
-    cat .judo/config.sh >> .judo/combined_config.sh
+    cp -u $JUDO_CONFIG_HOME/config.sh .judo/.cache/common_config.sh
+    cp -u .judo/config.sh .judo/.cache/project_config.sh
+    new_config=$(cat .judo/.cache/common_config.sh .judo/.cache/project_config.sh)
+    [ echo "$new_config" | cmp --silent .judo/.cache/combined_config.sh ] \
+        || echo "$new_config" > .judo/.cache/combined_config.sh
     datafiles=$(ls -d $DATA_ROOT/assets/*)
     datafile_names=$(ls $DATA_ROOT/assets)
-    echo $datafiles | sed 's/ /\n/g' | xargs -I{} cp -rp {} .judo
+    echo $datafiles | sed 's/ /\n/g' | xargs -I{} cp -rpu {} .judo/.cache
     touch requirements.txt && chmod 604 requirements.txt
+    cp -u requirements.txt .judo/.cache/requirements.txt
     if [ ! -d ".git" ]; then
         git init
         # Get up-to-date gitignore file for a jupyter notebook.
@@ -23,11 +27,8 @@ function init()
     if [ ! -z $image ]; then
         alternative_image="--build-arg BASE_IMAGE=$image"
     fi
-    docker build -t $project_name $alternative_image .judo
-    cd .judo
-    rm -rf plugins_dir
-    rm combined_config.sh
-    echo $datafile_names | sed 's/ /\n/g' | xargs rm -r
+    docker build -t $project_name $alternative_image .judo/.cache
+    #rm -rf plugins_dir
 }
 
 
