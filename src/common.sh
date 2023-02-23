faddee9edb19e3c12887b7fc962ee8d9824c9c63
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-02-23 15:02:03 +0100

    FEAT: Options and Help for judo-init

--- src/common.sh
+++ src/common.sh
@@ -4,7 +4,7 @@ function _show_opt()
 {
     option=$1
     summary=$2
-    echo -e $summary | awk -v o=$option -v p=$padding 'BEGIN{printf "%-"p"s", o};
+    echo -e $summary | awk -v o="$option" -v p=$padding 'BEGIN{printf "%-"p"s", o};
         NR==1{print $0};
         NR>1 {printf "%-"p"s", ""; print $0}'
 
@@ -29,7 +29,7 @@ function show_help()
         echo -e "\nCommands:"
     fi
     while [ $# -gt 0 ]; do
-        if [ $1 = '--' ]; then
+        if [ "$1" = '--' ]; then
             echo -e "\nCommands:"
             padding=16
             shift 1
--- src/init.sh
+++ src/init.sh
@@ -3,17 +3,16 @@
 function init()
 {
     mkdir -p .judo/.cache .judo/.cache/plugins_dir
-    # Get active plugins and copy to image cache
+    # Get active plugins and copy to image cache if updated
     source $DATA_ROOT/src/plugins.sh; echo $(_active_plugins) | xargs -I{} cp -rpu $DATA_ROOT/plugins/{} .judo/.cache/plugins_dir
     touch .judo/config.sh .judo/plugins
-    cp -u $JUDO_CONFIG_HOME/config.sh .judo/.cache/common_config.sh
+    if [ -z $ignore_global_config ]; then
+        cp -u $JUDO_CONFIG_HOME/config.sh .judo/.cache/common_config.sh
+    else
+        touch .judo/.cache/common_config.sh
+    fi
     cp -u .judo/config.sh .judo/.cache/project_config.sh
-    new_config=$(cat .judo/.cache/common_config.sh .judo/.cache/project_config.sh)
-    [ echo "$new_config" | cmp --silent .judo/.cache/combined_config.sh ] \
-        || echo "$new_config" > .judo/.cache/combined_config.sh
-    datafiles=$(ls -d $DATA_ROOT/assets/*)
-    datafile_names=$(ls $DATA_ROOT/assets)
-    echo $datafiles | sed 's/ /\n/g' | xargs -I{} cp -rpu {} .judo/.cache
+    find $DATA_ROOT/assets -exec cp -rpu {} .judo/.cache \;
     touch requirements.txt && chmod 604 requirements.txt
     cp -u requirements.txt .judo/.cache/requirements.txt
     if [ ! -d ".git" ]; then
@@ -28,54 +27,44 @@ function init()
         alternative_image="--build-arg BASE_IMAGE=$image"
     fi
     docker build -t $project_name $alternative_image .judo/.cache
-    #rm -rf plugins_dir
 }
 
 
 function help()
 {
-    echo """
-    -n|--name: Specify the name of the Docker container. Default: The current directories name (lowercase).
-    -i|--image: Use alternative base-image, instead of default jupyter/scipy-notebook.
-    """
-}
-
-function parse_arguments()
-{
-    # Read input arguments.
-    LONGOPTS=port:,name:,image:
-    OPTIONS=p:n:i:
-
-    ARGUMENTS=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name $0 -- $@)
-    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-        # getopt: wrong arguments
-        exit 2
-    fi
-    eval set -- "$ARGUMENTS"
-
-    while true; do
-        case $1 in
-            -h|--help)
-                help=1
-                shift 1
-                ;;
-            -n|--name)
-                project_name=$2
-                shift 2
-                ;;
-            -i|--image)
-                image=$2
-                shift 2
-                ;;
-            --)
-                shift
-                break
-                ;;
-        esac
-    done
+    source $DATA_ROOT/src/common.sh
+    show_help "judo init" "Initialize a judo-project in the current directory." \
+        "-n|--name name" "Specify the name of the Docker container. Default: The current directories name (lowercase)." \
+        "-i|--image name" "Use specific base-image. Default: jupyter/scipy-notebook." \
+        "--ignore_global_config" "Whether to ignore the global configuration file in .config/judo/config.sh." "--" \
+        "help" "View this help."
 }
 
 function exec_init()
 {
-    init $@
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -i|--image)
+                image=$2
+                shift 2
+                ;;
+
+            -n|--name)
+                project_name=$2
+                shift 2
+                ;;
+
+            --ignore_global_config)
+                ignore_global_config=1
+                shift 1
+                ;;
+
+            help|*)
+                help
+                shift 1
+                return
+                ;;
+        esac
+    done
+    init
 }
